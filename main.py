02a68e4dd0beaccc4a81e0083788c91fc4fccf77
Tynash <89925268+TinasheGuni@users.noreply.github.com>
2024-03-20 15:01:31 +0200

    routine practice

--- main.py
+++ main.py
@@ -1,66 +1,23 @@
 # import the necessary modules
-import random
 import sys
-from PyQt6.QtWidgets import (QApplication, QMainWindow, QVBoxLayout, QPushButton, QWidget,
-                             QLabel, QRadioButton)
-from PyQt6.QtCore import Qt, QSize
-from PyQt6.QtGui import QIcon
+from PyQt6.QtWidgets import *
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QPixmap
+from style_sheet import *
 
-class MainWindow(QMainWindow):
+class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.initializeUI()
 
     def initializeUI(self):
-        self.setWindowTitle("Guessing Game")
-        self.setWindowIcon(QIcon("resources/pyqt_logo.png"))
+        self.setMinimumSize(700, 700)
+        self.setWindowTitle("Food Order GUI")
 
-        self.setUpMainWindow()
         self.show()
 
-    def setUpMainWindow(self):
-        lbl_instruction = QLabel("<b>Guess the fruit</b>")
-
-        # choice
-        rbtn_apple = QRadioButton("Apple")
-        rbtn_pineapple = QRadioButton("Pineapple")
-        rbtn_watermelon = QRadioButton("Watermelon")
-        rbtn_banana = QRadioButton("Banana")
-
-        lbl_info = QLabel("Click the pushbutton to select an icon")
-
-        self.images = [
-            "resources/1_apple.png",
-            "resources/2_pineapple.png",
-            "resources/3_watermelon.png",
-            "resources/4_banana.png",
-        ]
-
-        self.btn_icon = QPushButton()
-        self.btn_icon.setIconSize(QSize(60, 60))
-        self.btn_icon.setIcon(QIcon(random.choice(self.images)))
-        self.btn_icon.clicked.connect(self.changeIcon)
-
-        lyt_main = QVBoxLayout()
-        lyt_main.addWidget(lbl_instruction)
-
-        lyt_main.addWidget(rbtn_apple)
-        lyt_main.addWidget(rbtn_pineapple)
-        lyt_main.addWidget(rbtn_watermelon)
-        lyt_main.addWidget(rbtn_banana)
-
-        lyt_main.addWidget(lbl_info)
-        lyt_main.addWidget(self.btn_icon)
-
-        container = QWidget()
-        container.setLayout(lyt_main)
-
-        self.setCentralWidget(container)
-
-    def changeIcon(self):
-        self.btn_icon.setIcon(QIcon(random.choice(self.images)))
-
-if __name__=="__main__":
+if __name__=='__main__':
     app = QApplication(sys.argv)
+    app.setStyleSheet(style)
     window = MainWindow()
     sys.exit(app.exec())
---
+++ style_sheet.py
@@ -0,0 +1,11 @@
+import sys
+
+style = """
+    QWidget {
+        background-color: #332255;
+    }
+    
+    QWidget#Tabs {
+        background-color: #FCEBCD
+    }
+"""
